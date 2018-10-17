dda5e7a800ad7be92dd669f1f6526178591866d1
jialechen131 <jialechen321@gmail.com>
2018-10-17 18:22:03 -0400

    zz

--- js/index.js
+++ js/index.js
@@ -4,3 +4,14 @@ import test from './script2.js';
 console.log('test');
 
 test();
+
+let database = firebase.database();
+let databaseRef = database.ref('/');
+
+databaseRef.once('value').then(function(snapshot)
+{
+const databaseValues = snapshot.val();
+
+console.log(databaseValues);
+document.write(databaseValues['dogs']);
+});
